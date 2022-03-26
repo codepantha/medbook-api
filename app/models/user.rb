86408919ffac8203ac3c66f0a50d5b7948758856
AlexRS90 <aramosbengy@gmail.com>
2022-03-25 21:38:49 -0600

    Add uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: { message: 'must not be empty' }
+  validates :name, presence: { message: 'Name must not be empty' }, uniqueness: true
   validates :name, length: { minimum: 5, maximum: 50, message: 'must be between 5 and 50 characters' }
 end
