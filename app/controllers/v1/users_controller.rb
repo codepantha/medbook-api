141cd300e54aba3328fdd6ecca6e2934635418f4
AlexRS90 <aramosbengy@gmail.com>
2022-03-25 21:40:05 -0600

    Add message to API responses

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -5,12 +5,23 @@ class V1::UsersController < ApplicationController
   end
 
   def show
-    user = User.find(params[:id])
-    render json: user.to_json
+      user = User.find_by_id(params[:id])
+    if user != nil
+      render json: user.to_json
+    else
+      render json: { status: 'error', code: 200, "message": "Couldn't find Quote with 'id'=#{params[:id]}" }
+    end
   end
 
   def create
-    new_user = User.create(name: params[:name])
-    render json: new_user.to_json
+    new_user = User.new(name: params[:name])
+    if new_user.save
+      render status: 201, json: {
+        message: "New user created successfully."
+      }
+    else
+      render json: { errors: new_user.errors,
+                     status: 422 }
+    end
   end
 end
