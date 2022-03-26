91ba6c16ca38317adffc74c27cfe6e39b0d704b1
micheaol <micheaol80@gmail.com>
2022-03-26 12:54:15 +0100

    Add users route to the appointment endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :appointments
+      resources :users do
+        resources :appointments
+      end
     end
+  end
+
   namespace :v1 do
     resources :users, only: [:index, :show, :create]
   end
