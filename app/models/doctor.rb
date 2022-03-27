b2e4c388fb96118c1d7487ee90ada07ee8566b42
codepanther <pezechigozirim@gmail.com>
2022-03-27 13:26:51 +0100

    add Doctor model validation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,10 @@
 class Doctor < ApplicationRecord
+  validates :name, presence: { message: 'is a required field' }
+  validates :specialty, presence: { message: 'is a required field' }
+  validates :date_of_birth, presence: { message: 'is a required field' }
+  validates :age, numericality: { only_integer: true }
+  validates :experience, presence: { message: 'is a required field' }
+  validates :experience, numericality: { only_integer: true }
+  validates :consultation_fee, presence: { message: 'is a required field' }
+  validates :consultation_fee, numericality: true
 end
