bdcd963f4c0c7ddae8a5e1cfb28d578cf37cd1f3
codepanther <pezechigozirim@gmail.com>
2022-03-27 15:51:58 +0100

    add create doctor endpoint and dynamically calculate age

--- app/controllers/v1/doctors_controller.rb
+++ app/controllers/v1/doctors_controller.rb
@@ -13,7 +13,31 @@ class V1::DoctorsController < ApplicationController
     end
   end
 
+  def create
+    doctor = Doctor.new(doctor_params)
+    doctor.age = age
+
+    if doctor.save
+      render json: doctor, status: 201
+    else
+      render json: { error: doctor.errors }, status: 422
+    end
+  end
+
+  private
+
   def set_doctor
     @doctor = Doctor.find_by_id(params[:id])
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :specialty, :profile_pic, :date_of_birth,
+                                   :experience, :consultation_fee, :bio)
+  end
+
+  def age
+    now = Time.current
+    dob = params[:doctor][:date_of_birth].to_time # change the string to time format
+    now.year - dob.year - ((now.month > dob.month) || (now.month == dob.month && now.day >= dob.day) ? 0 : 1)
   end
 end
