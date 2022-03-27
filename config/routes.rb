ffc34742590917e67d99ef4476662f3d12fc45d5
AlexRS90 <aramosbengy@gmail.com>
2022-03-26 21:04:11 -0600

    Refactor routes for user endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users do
+      resources :users, only: [:index, :show, :create] do
         resources :appointments
       end
     end
-  end
-
-  namespace :v1 do
-    resources :users, only: [:index, :show, :create]
   end
 end
