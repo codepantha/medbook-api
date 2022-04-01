17083ff273019ad38897ad4214754f27ac993119
codepanther <pezechigozirim@gmail.com>
2022-04-01 22:30:08 +0100

    add documentation for appointments endpoints

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'factory_bot_rails'
+  gem 'pry'
   gem 'rspec-rails', '~> 5.0.0'
   gem 'rswag-specs'
 end
--- spec/requests/api/v1/doctors_spec.rb
+++ spec/requests/api/v1/doctors_spec.rb
@@ -1,8 +1,8 @@
 require 'swagger_helper'
 
-RSpec.describe 'api/v1/users', type: :request do
-  path '/api/v1/users' do
-    get('list users') do
+RSpec.describe 'api/v1/doctors', type: :request do
+  path '/api/v1/doctors' do
+    get('list doctors') do
       response(200, 'successful') do
         after do |example|
           example.metadata[:response][:content] = {
@@ -15,7 +15,7 @@ RSpec.describe 'api/v1/users', type: :request do
       end
     end
 
-    post('create user') do
+    post('create doctor') do
       response(200, 'successful') do
         after do |example|
           example.metadata[:response][:content] = {
@@ -29,11 +29,26 @@ RSpec.describe 'api/v1/users', type: :request do
     end
   end
 
-  path '/api/v1/users/{id}' do
+  path '/api/v1/doctors/{id}' do
     # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
-    get('show user') do
+    get('show doctor') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete doctor') do
       response(200, 'successful') do
         let(:id) { '123' }
 
